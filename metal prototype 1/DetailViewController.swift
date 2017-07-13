8559c7e509b6dfbec95dd0d5de1f2432b97ee186
Daniel Pasco <daniel@blackpixel.com>
2017-07-13 15:59:15 -0700

    Now using shaders and a basic rendering pipeline

--- metal prototype 1/DetailViewController.swift
+++ metal prototype 1/DetailViewController.swift
@@ -10,9 +10,8 @@
 
 import UIKit
 import MetalKit
-class DetailViewController: UIViewController {
 
-//class DetailViewController: UIViewController, MTKViewDelegate {
+class DetailViewController: UIViewController, MTKViewDelegate {
 
     @IBOutlet weak var detailDescriptionLabel: UILabel!
     var metalView: MTKView!
@@ -22,89 +21,110 @@ class DetailViewController: UIViewController {
     var secondTexture: MTLTexture?
 
     /// Metal device
-    var defaultMetalDevice = MTLCreateSystemDefaultDevice()
+    var metalDevice = MTLCreateSystemDefaultDevice()
     
     /// Metal pipeline state we use for rendering
     var renderPipelineState: MTLRenderPipelineState?
     
-    func initMetal() {
-        
-        let debugMetalView = MTKView(frame: self.view.bounds, device: defaultMetalDevice )
-        self.metalView = debugMetalView
-        self.metalCommandQueue = defaultMetalDevice?.makeCommandQueue()
-        
-        //        self.metalView.delegate = self
-        self.metalView.framebufferOnly = false
-//        self.metalView.device = defaultMetalDevice
-        self.metalView.contentMode = .scaleAspectFit
-        self.metalView.autoResizeDrawable = true
-        self.metalView.contentScaleFactor = UIScreen.main.scale
-        self.metalView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        self.metalView.colorPixelFormat = MTLPixelFormat.bgra8Unorm_srgb
-        self.view.insertSubview(self.metalView, at: 0)
+    func loadTexture(name:String)->MTLTexture? {
+        let image = UIImage(named:name)
+        let cgImage = image?.cgImage
+        var texture:MTLTexture?
+        do {
+            texture = try MTKTextureLoader(device: metalDevice!).newTexture(with: cgImage!, options: nil)
+        }  catch let error as NSError {
+            print("[ERROR] - Failed to create texture. \(error)")
+        }
+        return texture
+    }
+
+    override func awakeFromNib() {
+        initMetal()
     }
     
-//    public func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
-//
-//    }
-//
-//    func draw(in view: MTKView) {
-//        guard
-//            let currentRenderPassDescriptor = metalView.currentRenderPassDescriptor,
-//            let currentDrawable = metalView.currentDrawable,
-//            let renderPipelineState = renderPipelineState
-//        else {
+    func initMetal() {
+        
+        let debugMetalView = MTKView(frame: view.bounds, device: metalDevice )
+        metalView = debugMetalView
+        metalCommandQueue = metalDevice?.makeCommandQueue()
+        
+        metalView.framebufferOnly = false
+        metalView.contentMode = .scaleAspectFit
+        metalView.autoResizeDrawable = true
+        metalView.contentScaleFactor = UIScreen.main.scale
+        metalView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        metalView.colorPixelFormat = MTLPixelFormat.bgra8Unorm_srgb
+        view.insertSubview(self.metalView, at: 0)
+        firstTexture = loadTexture(name: "0.png")
+        secondTexture = loadTexture(name: "0.png")
+        metalView.delegate = self
+
+        initializeRenderPipelineState()
+    }
+    
+    public func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+
+    }
+
+    open func didRenderTexture(_ texture: MTLTexture, withCommandBuffer commandBuffer: MTLCommandBuffer, device: MTLDevice) {
+        /**
+         * Override if neccessary
+         */
+    }
+    
+    func draw(in view: MTKView) {
+        guard
+            let currentRenderPassDescriptor = metalView.currentRenderPassDescriptor,
+            let currentDrawable = metalView.currentDrawable,
+            let renderPipelineState = renderPipelineState
+        else {
 //            semaphore.signal()
-//            return
-//        }
-//        let commandBuffer = self.metalCommandQueue.makeCommandBuffer()
-//
-//        let encoder = commandBuffer?.makeRenderCommandEncoder(descriptor: currentRenderPassDescriptor)
-//        encoder?.pushDebugGroup("RenderFrame")
-//        encoder.setRenderPipelineState(renderPipelineState)
-//        encoder.setFragmentTexture(texture, at: 0)
-//        encoder??.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4, instanceCount: 1)
-//        encoder?.popDebugGroup()
-//        encoder?.endEncoding()
-//
-//        commandBuffer.addScheduledHandler { [weak self] (buffer) in
-//            guard let unwrappedSelf = self else { return }
-//
-//            unwrappedSelf.didRenderTexture(texture, withCommandBuffer: buffer, device: device)
+            return
+        }
+        let commandBuffer = metalCommandQueue?.makeCommandBuffer()
+
+        let encoder = commandBuffer?.makeRenderCommandEncoder(descriptor: currentRenderPassDescriptor)
+        encoder?.pushDebugGroup("RenderFrame")
+        encoder?.setRenderPipelineState(renderPipelineState)
+        encoder?.setFragmentTexture(secondTexture, index: 0)
+        encoder?.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4, instanceCount: 1)
+        encoder?.popDebugGroup()
+        encoder?.endEncoding()
+
+        commandBuffer?.addScheduledHandler { [weak self] (buffer) in
+            guard let unwrappedSelf = self else { return }
+
+            unwrappedSelf.didRenderTexture(unwrappedSelf.secondTexture!, withCommandBuffer: buffer, device: unwrappedSelf.metalDevice!)
 //            unwrappedSelf.semaphore.signal()
-//        }
-//        commandBuffer.present(currentDrawable)
-//        commandBuffer.commit()
-//    }
+        }
+        commandBuffer?.present(currentDrawable)
+        commandBuffer?.commit()
+    }
 //
-//    fileprivate func initializeRenderPipelineState() {
-//        guard
-//            let device = device,
-//            let library = device.newDefaultLibrary()
-//            else { return }
-//
-//        let pipelineDescriptor = MTLRenderPipelineDescriptor()
-//        pipelineDescriptor.sampleCount = 1
-//        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
-//        pipelineDescriptor.depthAttachmentPixelFormat = .invalid
-//
-//        /**
-//         *  Vertex function to map the texture to the view controller's view
-//         */
-//        pipelineDescriptor.vertexFunction = library.makeFunction(name: "mapTexture")
-//        /**
-//         *  Fragment function to display texture's pixels in the area bounded by vertices of `mapTexture` shader
-//         */
-//        pipelineDescriptor.fragmentFunction = library.makeFunction(name: "displayTexture")
-//
-//        do {
-//            try renderPipelineState = device.makeRenderPipelineState(descriptor: pipelineDescriptor)
-//        }
-//        catch {
-//            assertionFailure("Failed creating a render state pipeline. Can't render the texture without one.")
-//            return
-//        }
-//    }
+    fileprivate func initializeRenderPipelineState() {
+        let library = metalDevice?.makeDefaultLibrary()
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.sampleCount = 1
+        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm_srgb
+        pipelineDescriptor.depthAttachmentPixelFormat = .invalid
+        
+        /**
+         *  Vertex function to map the texture to the view controller's view
+         */
+        pipelineDescriptor.vertexFunction = library?.makeFunction(name: "mapTexture")
+        /**
+         *  Fragment function to display texture's pixels in the area bounded by vertices of `mapTexture` shader
+         */
+        pipelineDescriptor.fragmentFunction = library?.makeFunction(name: "displayTexture")
+        
+        do {
+            try renderPipelineState = metalDevice?.makeRenderPipelineState(descriptor: pipelineDescriptor)
+        }
+        catch {
+            assertionFailure("Failed creating a render state pipeline. Can't render the texture without one.")
+            return
+        }
+    }
 
     func configureView() {
         // Update the user interface for the detail item.
@@ -113,34 +133,11 @@ class DetailViewController: UIViewController {
                 label.text = detail.description
             }
         }
-        initMetal()
-        renderImage()
     }
 
-    func loadTexture(name:String)->MTLTexture? {
-        let image = UIImage(named:name)
-        let cgImage = image?.cgImage
-        var texture:MTLTexture?
-        do {
-            texture = try MTKTextureLoader(device: defaultMetalDevice!).newTexture(with: cgImage!, options: nil)
-        }  catch let error as NSError {
-            print("[ERROR] - Failed to create texture. \(error)")
-        }
-        return texture
-    }
-    
     func renderImage() {
         
-        if self.detailItem != nil {
-            self.firstTexture = loadTexture(name: self.detailItem!)
-            self.secondTexture = loadTexture(name: self.detailItem!)
-        }
-        else {
-            self.firstTexture = loadTexture(name: "0.png")
-            self.secondTexture = loadTexture(name: "0.png")
-        }
         let texture:MTLTexture? = self.secondTexture
-//        self.metalView.drawableSize = CGSize(width: (texture?.width)!, height: (texture?.height)!)
         let commandBuffer = self.metalCommandQueue?.makeCommandBuffer()
         let blitEncoder = commandBuffer?.makeBlitCommandEncoder()
         let drawable = self.metalView.currentDrawable
@@ -172,6 +169,10 @@ class DetailViewController: UIViewController {
         didSet {
             // Update the view.
             configureView()
+            if detailItem != nil {
+                firstTexture = loadTexture(name: detailItem!)
+                secondTexture = loadTexture(name: detailItem!)
+            }
         }
     }
 }
